6cf7e8c85dc9203932e1dd1e20c9fcc8c1ad5e25
juefried <jue@gmx.de>
2024-07-20 21:59:11 +0200

    errorhandling verbessert

--- data_grabber/libs/cache_thinning_lib.py
+++ data_grabber/libs/cache_thinning_lib.py
@@ -5,6 +5,7 @@ import diskcache as dc
 cache = dc.Cache('cache')
 
 def delete_random_cache_data(cache_name, percentage):
+    # Collect all keys that belong to the named cache
     named_cache_keys = [key for key in cache.iterkeys() if isinstance(key, tuple) and key[0] == cache_name]
 
     total_items = len(named_cache_keys)
@@ -19,7 +20,12 @@ def delete_random_cache_data(cache_name, percentage):
     keys_to_delete = random.sample(named_cache_keys, num_to_delete)
 
     # Schlüssel löschen
+    deleted_count = 0
     for key in keys_to_delete:
-        del cache[key]
+        try:
+            del cache[key]
+            deleted_count += 1
+        except KeyError:
+            print(f"Key {key} not found. Skipping deletion.")
 
-    print(f"{num_to_delete} items deleted from named cache '{cache_name}'.")
+    print(f"{deleted_count} items deleted from named cache '{cache_name}'.")
