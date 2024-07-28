5d8bdd3ebde52312b1ba893abfbaaaa6f5128ba8
juefried <jue@gmx.de>
2024-07-28 15:23:37 +0200

    Prio für Adressen mit type:postal_code eingebaut

--- data_grabber/libs/location_nominatim_lib.py
+++ data_grabber/libs/location_nominatim_lib.py
@@ -38,7 +38,14 @@ def query_nominatim(searchstring, country_code):
             response.raise_for_status()
             data = response.json()
             if data:
-                return next((entry for entry in data if entry['type'] == 'administrative'), data[0])
+                for entry in data:
+                    if entry['type'] == 'administrative':
+                        return entry
+                for entry in data:
+                    if entry['type'] =='postal_code':
+                        return entry
+                return data[0]
+
     except requests.RequestException as e:
         print(f"An error occurred while fetching location details for \"{searchstring}\", country_code={country_code}: {e}")
     return None
