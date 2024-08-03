7f97aa2dc4e6dd89c1157615fd0e9fa8f9e3840c
juefried <jue@gmx.de>
2024-08-03 08:26:32 +0200

    some commenting and refactoring

--- data_grabber/utils/delete_user_cash_entry.py
+++ data_grabber/utils/delete_user_cash_entry.py
@@ -1,35 +1,50 @@
 #!/usr/bin/python3
 
 import argparse
-import diskcache as dc
 import os
+import diskcache as dc
+
 
 def main():
-    # Argument parser für die Befehlszeilenparameter
-    parser = argparse.ArgumentParser(description='Lösche eine uid aus einem benannten Cache.')
-    parser.add_argument('uid', type=str, help='Die uid, die gelöscht werden soll')
+    """
+    Main function to delete a user's details from a named cache.
 
+    This function takes a user's unique identifier (uid) as a command line argument and deletes the corresponding
+    entry from a named cache.
+    """
+
+    # Create an argument parser for command line parameters
+    parser = argparse.ArgumentParser(description='Delete a user\'s details from a named cache.')
+
+    # Add an argument for the uid
+    parser.add_argument('uid', type=str, help='The unique identifier of the user whose details should be deleted.')
+
+    # Parse the command line arguments
     args = parser.parse_args()
 
+    # Get the uid from the command line arguments
     uid = args.uid
-    cache_name = 'user_details'  # Fester Name des Caches
 
-    # Berechne den Pfad zum Cache relativ zum Skript
+    # Set the name of the cache
+    cache_name = 'user_details'
+
+    # Calculate the path to the cache relative to the script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     cache_path = os.path.join(script_dir, '../cache')
 
-    # Öffne den benannten Cache
+    # Open the named cache
     cache = dc.Cache(cache_path)
 
-    # Schlüssel für die uid im Cache
-    key = ('user_details', uid)
+    # Key for the uid in the cache
+    key = (cache_name, uid)
 
-    # Lösche die uid aus dem Cache
+    # Delete the uid from the cache
     if key in cache:
         del cache[key]
-        print(f'uid "{uid}" wurde aus dem Cache "{cache_name}" gelöscht.')
+        print(f'uid "{uid}" was deleted from the cache "{cache_name}".')
     else:
-        print(f'uid "{uid}" nicht im Cache "{cache_name}" gefunden.')
+        print(f'uid "{uid}" was not found in the cache "{cache_name}".')
+
 
 if __name__ == '__main__':
     main()
